d38a52d8d29d3b5c0d4cc523bc135ca24736098e
raj-ranjan-01 <131339307+raj-ranjan-01@users.noreply.github.com>
2023-12-13 23:31:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def find_largest_number(number1,number2,number3):
   return max(number1, number2, number3)
 
 st.title('Find the Largest Number')
+st.write("Enter three numbers below:")
 
 number1 = st.number_input('Enter first number')
 number2 = st.number_input('Enter second number')
